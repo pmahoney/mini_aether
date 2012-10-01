e25b57df90c72f0c124a8100863987b71ce60c35
Patrick Mahoney <pat@polycrystal.org>
2012-10-01 08:47:47 -0500

    Remove obsolete files.

--- old/test-mini.rb
+++
@@ -1,8 +0,0 @@
-require 'mini_aether'
-
-MiniAether.setup do
-  # source 'http://clojars.org/repo'
-  # jar 'noir-servlet:noir-servlet:0.1.0'
-  # jar 'org.slf4j:slf4j-api:1.6.1'
-  jar 'org.jboss.resteasy:resteasy-jaxrs:2.3.4.Final'
-end
